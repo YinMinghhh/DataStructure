1089c1b44829165abe866b23b5c83fb7aafa538d
YM <95911414+YinMinghhh@users.noreply.github.com>
2023-03-25 12:13:05 +0800

    ymStack风格跟ymString统一了

--- lib/ymStack.h
+++ lib/ymStack.h
@@ -31,80 +31,184 @@ int main() {
 #include <errno.h>
 #include <string.h>
 
-/**
- * easy reference
- */
-#define _stack(typename) _stack$_##typename##_$
-#define stack(typename) _stack(typename)
-
-/**
- * template
- */
-#define _stack_DEF(typename) typedef        \
-struct stack$_##typename##_$ {              \
-    typename element;                       \
-    struct stack$_##typename##_$ * next;    \
-} stack(typename);                          \
-                                            \
-void stack_init$_##typename##_$ (stack(typename) * const this) {                    \
-    this->next = NULL;}                     \
-    \
-bool stack_push$_##typename##_$ (stack(typename) * const this, typename element) {  \
-    stack(typename) * node = (stack(typename) *)malloc(sizeof (stack(typename)));   \
-    if (node == NULL) {                     \
-        fprintf(stderr, "Value of errno: %d\n", errno);                             \
-        fprintf(stderr, "Error stack_push(%s): %s\n", #typename, strerror(errno));  \
-        exit(EXIT_FAILURE);                 \
-    }                                       \
-    node->element = element;                \
-    node->next = this->next;                \
-    this->next = node;                      \
-    return true;}                           \
-    \
-bool stack_empty$_##typename##_$ (const stack(typename) * const this) {             \
-    return this->next == NULL;}             \
-    \
-typename stack_pop$_##typename##_$ (stack(typename) * const this) {                 \
-    stack(typename) * temp_node = this->next;                    \
-    typename temp_element = temp_node->element;                  \
-    this->next = this->next->next;          \
-    free(temp_node);                        \
-    return temp_element;}                   \
-    \
-typename stack_top$_##typename##_$ (const stack(typename) * const this) {           \
-    if (!stack_empty(typename)(this))       \
-        return this->next->element;         \
-    else {                                  \
-        fprintf(stderr, "Value of errno: %d\n", errno);                             \
-        fprintf(stderr, "Error stack_top(%s): %s\n", #typename, strerror(errno));   \
-        exit(EXIT_FAILURE);                 \
-    }}
-
-#define stack_def(typename) _stack_DEF(typename)
-
-/**
- * easy references for functions
- */
-#define stack_init(typename) stack_init$_##typename##_$
-#define stack_push(typename) stack_push$_##typename##_$
-#define stack_empty(typename) stack_empty$_##typename##_$
-#define stack_pop(typename) stack_pop$_##typename##_$
-#define stack_top(typename) stack_top$_##typename##_$
-
-/**
- * test
- */
-//stack_def(int)
-
-//#define _STACK_TEMPLATE_(typename) typedef \
-//struct STACK_$##typename##$                \
-//{                                          \
-//    typename element;                      \
-//    struct STACK_$##typename##$ *next;     \
-//} stack(typename); \
+///**
+// * easy reference
+// */
+//#define _stack(typename) _stack$_##typename##_$
+//#define stack(typename) _stack(typename)
 //
-//#define stack_instantiation(typename) _STACK_TEMPLATE_(typename)
+///**
+// * template
+// */
+//#define _stack_DEF(typename) typedef        \
+//struct stack$_##typename##_$ {              \
+//    typename element;                       \
+//    struct stack$_##typename##_$ * next;    \
+//} stack(typename);                          \
+//                                            \
+//void stack_init$_##typename##_$ (stack(typename) * const this) {                    \
+//    this->next = NULL;}                     \
+//    \
+//bool stack_push$_##typename##_$ (stack(typename) * const this, typename element) {  \
+//    stack(typename) * node = (stack(typename) *)malloc(sizeof (stack(typename)));   \
+//    if (node == NULL) {                     \
+//        fprintf(stderr, "Value of errno: %d\n", errno);                             \
+//        fprintf(stderr, "Error stack_push(%s): %s\n", #typename, strerror(errno));  \
+//        exit(EXIT_FAILURE);                 \
+//    }                                       \
+//    node->element = element;                \
+//    node->next = this->next;                \
+//    this->next = node;                      \
+//    return true;}                           \
+//    \
+//bool stack_empty$_##typename##_$ (const stack(typename) * const this) {             \
+//    return this->next == NULL;}             \
+//    \
+//typename stack_pop$_##typename##_$ (stack(typename) * const this) {                 \
+//    stack(typename) * temp_node = this->next;                    \
+//    typename temp_element = temp_node->element;                  \
+//    this->next = this->next->next;          \
+//    free(temp_node);                        \
+//    return temp_element;}                   \
+//    \
+//typename stack_top$_##typename##_$ (const stack(typename) * const this) {           \
+//    if (!stack_empty(typename)(this))       \
+//        return this->next->element;         \
+//    else {                                  \
+//        fprintf(stderr, "Value of errno: %d\n", errno);                             \
+//        fprintf(stderr, "Error stack_top(%s): %s\n", #typename, strerror(errno));   \
+//        exit(EXIT_FAILURE);                 \
+//    }}
 //
-//stack_instantiation(char)
+//#define stack_def(typename) _stack_DEF(typename)
+//
+///**
+// * easy references for functions
+// */
+//#define stack_init(typename) stack_init$_##typename##_$
+//#define stack_push(typename) stack_push$_##typename##_$
+//#define stack_empty(typename) stack_empty$_##typename##_$
+//#define stack_pop(typename) stack_pop$_##typename##_$
+//#define stack_top(typename) stack_top$_##typename##_$
+//
+///**
+// * test
+// */
+////stack_def(int)
+
+#define ymStack_t(typename) STACK_$##typename##$
+#define ymStack(typename)   STACK_fun_$##typename##$
+
+#define ymStack_init(typename)  ymStack_$##typename##$_init
+#define ymStack_push(typename)  ymStack_$##typename##$_push
+#define ymStack_empty(typename) ymStack_$##typename##$_empty
+#define ymStack_top(typename)   ymStack_$##typename##$_top
+#define ymStack_pop(typename)   ymStack_$##typename##$_pop
+
+
+#define _STACK_TEMPLATE_(typename) typedef \
+struct STACK_$##typename##$                \
+{                                          \
+    typename element;                      \
+    struct STACK_$##typename##$ *next;     \
+} ymStack_t(typename);                     \
+                                           \
+\
+static ymStack_t(typename) *ymStack_$##typename##$_init (ymStack_t(typename) *this); \
+static bool ymStack_$##typename##$_push(ymStack_t(typename) *this, typename element);\
+static bool ymStack_$##typename##$_empty(const ymStack_t(typename) * this);          \
+static typename ymStack_$##typename##$_top(const ymStack_t(typename) * this);        \
+static typename ymStack_$##typename##$_pop(ymStack_t(typename) * this);              \
+\
+                                           \
+struct {                                   \
+    ymStack_t(typename) *(*init)(ymStack_t(typename)*);                              \
+    bool    (*push)     (ymStack_t(typename)*, typename);                            \
+    bool    (*empty)    (const ymStack_t(typename)*);                                \
+    typename(*top)      (const ymStack_t(typename)*);                                \
+    typename(*pop)      (ymStack_t(typename)*);                                      \
+    \
+} ymStack(typename) = {                    \
+    .init   =   ymStack_init    (typename),\
+    .push   =   ymStack_push    (typename),\
+    .empty  =   ymStack_empty   (typename),\
+    .top    =   ymStack_top     (typename),\
+    .pop    =   ymStack_pop     (typename),\
+};                       \
+                                           \
+static ymStack_t(typename) *ymStack_$##typename##$_init (ymStack_t(typename) *const this) \
+{                                          \
+    this->next = NULL;                     \
+    return this;                           \
+}                                          \
+                                           \
+static bool ymStack_$##typename##$_push(ymStack_t(typename) *const this, typename element)\
+{                                          \
+    ymStack_t(typename) *new_node = (ymStack_t(typename)*) malloc( \
+    sizeof (ymStack_t(typename)));         \
+    if (new_node == NULL) goto MallocError;\
+    else;                                  \
+    new_node->element = element;           \
+    new_node->next = this->next;           \
+    this->next = new_node;                 \
+    return true;                           \
+                                           \
+    MallocError:                           \
+    fprintf( stderr, "Error %s %s: malloc error\n", __FILE__, __FUNCTION__ ); \
+    goto finally;                          \
+    finally:                               \
+    exit(EXIT_FAILURE);\
+}                                          \
+                                           \
+static bool ymStack_$##typename##$_empty(const ymStack_t(typename) *const this)           \
+{                                          \
+    return this->next == NULL;             \
+}                                          \
+                                           \
+static typename ymStack_$##typename##$_top(const ymStack_t(typename) *const this)         \
+{                                          \
+    if (!ymStack_empty(typename)(this)) {  \
+        return this->next->element;        \
+    } else goto EmptyStackError;           \
+                                           \
+    EmptyStackError:                       \
+    fprintf( stderr, "Error %s %s: empty stack error\n", __FILE__, __FUNCTION__ );        \
+    goto finally;                          \
+    finally:                               \
+    exit(EXIT_FAILURE);                    \
+}                                          \
+                                           \
+static typename ymStack_$##typename##$_pop(ymStack_t(typename) *const this)               \
+{                                          \
+    if (ymStack_empty(typename)(this)) goto EmptyStackError;                              \
+    else;                                  \
+    ymStack_t(typename) *tmp_node = this->next;                                           \
+    typename tmp_element = tmp_node->element;                                             \
+    this->next = this->next->next;         \
+    if (tmp_node != NULL) { free(tmp_node); tmp_node = NULL; }                            \
+    else;                                  \
+    return tmp_element;                    \
+                                           \
+    EmptyStackError:                       \
+    fprintf( stderr, "Error %s %s: empty stack error\n", __FILE__, __FUNCTION__ );        \
+    goto finally;                          \
+    finally:                               \
+    exit(EXIT_FAILURE);                    \
+}\
+
+
+#define stack_instantiation(typename) _STACK_TEMPLATE_(typename)
+
+stack_instantiation(char)
+stack_instantiation(int)
+stack_instantiation(float)
+stack_instantiation(double)
+
+//#undef ymStack_empty
+//#undef ymStack_init
+//#undef ymStack_push
+//#undef ymStack_empty
+//#undef ymStack_top
+//#undef ymStack_pop
 
 #endif //DATASTRUCT01_YMSTACK_H
--- main.c
+++ main.c
@@ -1,69 +1,14 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include "lib/ymStack.h"
-#include "lib/ymSeqList.h"
 #include "lib/ymString.h"
 
-stack_def(char)
-
 int main(int argc, char **argv) {
-    char expression[100];
-    char buffer[100];
-    unsigned long long length; stack(char) stack;
-    int i = 0, j = 0;   // i -> expression, j -> buffer
-    scanf("%s", expression);
-    length = strlen(expression);
-    stack_init(char)(&stack);
+    ymString_t *buffer = NULL, *expression = NULL;
+    expression  =   ymString.init(expression, 10);
+    buffer      =   ymString.init(buffer, 10);
+    ymString.gets(expression);
+//    ymString.puts(expression);
 
-    for (i = 0; i < length; i++) {
-        char element = expression[i];
-        if (element >= '0' && element <= '9') {
-            buffer[j++] = element;
-        } else if (element == '(') {
-            stack_push(char)(&stack, element);
-        } else if (element == ')') {
-            char operator0;
-            if (!stack_empty(char)(&stack)) {
-                operator0 = stack_top(char)(&stack);
-                while (operator0 != '(') {
-                    buffer[j++] = stack_pop(char)(&stack);
-                    if (!stack_empty(char)(&stack))
-                        operator0 = stack_top(char)(&stack);
-                }
-            }
-            stack_pop(char)(&stack);
-        } else if (element == '*' || element == '/') {
-            char operator1;
-            if (!stack_empty(char)(&stack)) {
-                operator1 = stack_top(char)(&stack);
-                while (operator1 == '*' || operator1 == '/') {
-                    buffer[j++] = stack_pop(char)(&stack);
-                    if (!stack_empty(char)(&stack))
-                        operator1 = stack_top(char)(&stack);
-                }
-            }
-            stack_push(char)(&stack, element);
-        } else if (element == '+' || element == '-') {
-            char operator2;
-            if (!stack_empty(char)(&stack)) {
-                operator2 = stack_top(char)(&stack);
-                while (operator2 == '*' || operator2 == '/' || operator2 == '+' || operator2 == '-') {
-                    buffer[j++] = stack_pop(char)(&stack);
-                    if (!stack_empty(char)(&stack))
-                        operator2 = stack_top(char)(&stack);
-                }
-            }
-            stack_push(char)(&stack, element);
-        } else {
-            buffer[j++] = element;
-        }
-    }
-    while (!stack_empty(char)(&stack)) {
-        buffer[j++] = stack_pop(char)(&stack);
-    }
-    for (int k = 0; k < j; ++k) {
-        putchar(buffer[k]);
-    }
+    ;
     putchar('\n');
     system("pause");
     return 0;
